bd9fcc6108cdabc74accade79b78cbc00a810849
Sub4ik <zerotanker33@gmail.com>
2023-10-09 13:08:57 +0300

    NetworkManager

--- app/src/main/java/ru/chatan/app/MainActivity.kt
+++ app/src/main/java/ru/chatan/app/MainActivity.kt
@@ -1,32 +1,95 @@
 package ru.chatan.app
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.transitions.SlideTransition
 import ru.chatan.app.presentation.start.StartScreen
 import ru.chatan.app.presentation.theme.ChatanTheme
+import ru.chatan.app.presentation.welcome.WelcomeScreen
 import ru.chatan.app.presentation.welcome.WelcomeView
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalAnimationApi::class)
 class MainActivity : ComponentActivity() {
+
+    private var networkManager: NetworkManager? = null
+
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         WindowCompat.setDecorFitsSystemWindows(window, false)
+        networkManager?.connect(activity = this@MainActivity)
 
         setContent {
+            val snackbarHostState = remember { SnackbarHostState() }
+            val network by remember { mutableStateOf(networkManager?.getNetworkState()) }
+
+            CheckNetworkState(snackbarHostState, network)
+
             ChatanTheme {
-                Navigator(StartScreen()) {
-                    SlideTransition(it)
+                Scaffold(
+                    snackbarHost = {
+                        SnackbarHost(hostState = snackbarHostState)
+                    }
+                ) {
+                    Navigator(
+                        screen = StartScreen(),
+                        onBackPressed = { currentScreen ->
+                            when (currentScreen) {
+                                is WelcomeScreen -> {
+                                    finish()
+                                    false
+                                }
+
+                                else -> true
+                            }
+                        }
+                    ) {
+                        SlideTransition(it)
+                    }
                 }
             }
         }
+    }
+
+    @Composable
+    private fun CheckNetworkState(
+        snackbarHostState: SnackbarHostState,
+        network: Boolean?
+    ) {
+        LaunchedEffect(key1 = snackbarHostState) {
+            if (network == false)
+                snackbarHostState.showSnackbar(
+                    message = getString(R.string.network_unavailable),
+                    duration = SnackbarDuration.Indefinite,
+                    withDismissAction = true
+                )
+            else snackbarHostState.currentSnackbarData?.dismiss()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        networkManager?.stop()
+        networkManager = null
     }
 }
 
---
+++ app/src/main/java/ru/chatan/app/NetworkManager.kt
@@ -0,0 +1,46 @@
+package ru.chatan.app
+
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+
+class NetworkManager {
+
+    private var networkState = true
+    private var connectivityManager: ConnectivityManager? = null
+
+    fun getNetworkState() = networkState
+
+    private val networkRequest = NetworkRequest.Builder()
+        .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+        .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+        .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+        .build()
+
+    private val networkCallback = object : ConnectivityManager.NetworkCallback() {
+        override fun onAvailable(network: Network) {
+            super.onAvailable(network)
+
+            networkState = false
+        }
+
+        override fun onLost(network: Network) {
+            super.onLost(network)
+
+            networkState = true
+        }
+    }
+
+    fun connect(
+        activity: MainActivity
+    ) {
+        connectivityManager = activity.getSystemService(ConnectivityManager::class.java) as ConnectivityManager
+        connectivityManager?.requestNetwork(networkRequest, networkCallback)
+    }
+
+    fun stop() {
+        connectivityManager = null
+    }
+
+}
